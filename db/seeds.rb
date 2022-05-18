ae5c558c88643c605ab67bf56d04713f5be40082
Joana Mena <joanamacarenamg@gmail.com>
2022-05-18 16:46:44 +0200

    Last push, didn't finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "starting"
+4.times do
+  flat = Flat.create!(
+    name: Faker::Book.title,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraphs(number: 3).join(' '),
+    price_per_night: rand(1..100),
+    number_of_guests: rand(1..10)
+  )
+  p flat
+end
